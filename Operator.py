5704355ffa3801d9327b54eb876afb4c5cc5a64f
12953 Bao <12953 Bao@vdm.mabuchi-motor.co.jp>
2023-11-07 12:13:58 +0700

    tét

--- Operator.py
+++ Operator.py
@@ -20,3 +20,84 @@ print(x / y)
 x = 5
 y = 2
 print(x % y)
+# **	Exponentiation	x ** y
+x = 2
+y = 5
+print(x ** y) #same as 2*2*2*2*2
+# //	Floor division	x // y
+x = 15
+y = 2
+print(x // y)
+#the floor division // rounds the result down to the nearest whole number
+
+print("-*********************** Python Assignment Operators")
+# =	x = 5	x = 5
+x = 5
+print(x)
+
+# +=	x += 3	x = x + 3
+x = 5
+x += 3
+print(x)
+
+# -=	x -= 3	x = x - 3
+x = 5
+x -= 3
+print(x)
+
+# *=	x *= 3	x = x * 3
+x = 5
+x *= 3
+print(x)
+
+# /=	x /= 3	x = x / 3
+x = 5
+x /= 3
+print(x)
+
+# %=	x %= 3	x = x % 3
+x = 5
+x%=3
+print(x)
+
+# //=	x //= 3	x = x // 3
+x = 5
+x//=3
+print(x)
+
+# **=	x **= 3	x = x ** 3
+x = 5
+x **= 3
+print(x)
+
+# &=	x &= 3	x = x & 3
+x = 5
+print (bin(x)) # 00000000000000000000000000000101
+x &= 3
+print(bin(3)) #00000000000000000000000000000011
+print("& : " + str(x)) #00000000000000000000000000000001
+
+# ~	    x = ~a
+x = 5
+x = ~x
+print("~ : " + str(x))
+
+# |=	x |= 3	x = x | 3
+x = 5
+x |= 3
+print("| : " + str(x))
+
+# ^=	x ^= 3	x = x ^ 3
+x = 5
+x ^= 3
+print("^ : " + str(x))
+
+# >>=	x >>= 3	x = x >> 3
+x = 5
+x >>= 3
+print(">> : " + str(x))
+
+# <<=	x <<= 3	x = x << 3
+x = 5
+x <<= 3
+print("<< : " + str(x))
