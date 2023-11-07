05a2789984630167f49fd0f4c9105f75d9ac00b1
12953 Bao <12953 Bao@vdm.mabuchi-motor.co.jp>
2023-11-07 08:26:36 +0700

    First Commit

---
+++ Operator.py
@@ -0,0 +1,22 @@
+# -***********************Operators
+print("-*********************** Python Arithmetic Operators")
+# +	Addition	x + y
+x = 5
+y = 3
+print(x + y)
+# -	Subtraction	x - y
+x = 5
+y = 3
+print(x - y)
+# *	Multiplication	x * y
+x = 5
+y = 3
+print(x * y)
+# /	Division	x / y
+x = 12
+y = 3
+print(x / y)
+# %	Modulus	x % y
+x = 5
+y = 2
+print(x % y)
