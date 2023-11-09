61e43a4a7f76e60793e6c8499ca419f700588e24
12953 Bao <12953 Bao@vdm.mabuchi-motor.co.jp>
2023-11-09 16:38:52 +0700

    a

--- Sets.py
+++ Sets.py
@@ -169,3 +169,109 @@ x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
 x.difference_update(y)
 print(x)
+
+# discard()
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+print(fruits)
+
+# intersection()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)
+
+x = {"a", "b", "c"}
+y = {"c", "d", "e"}
+z = {"f", "g", "c"}
+result = x.intersection(y, z)
+print(result)
+
+# intersection_update()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.intersection_update(y)
+print(x)
+
+x = {"a", "b", "c"}
+y = {"c", "d", "e"}
+z = {"f", "g", "c"}
+x.intersection_update(y, z)
+print(x)
+
+# isdisjoint()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "facebook"}
+z = x.isdisjoint(y)
+print(z)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.isdisjoint(y)
+print(z)
+
+# issubset()
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b", "a"}
+z = x.issubset(y)
+print(z)
+
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b"}
+z = x.issubset(y)
+print(z)
+
+# issuperset()
+x = {"f", "e", "d", "c", "b", "a"}
+y = {"a", "b", "c"}
+z = x.issuperset(y)
+print(z)
+
+x = {"f", "e", "d", "c", "b"}
+y = {"a", "b", "c"}
+z = x.issuperset(y)
+print(z)
+
+# pop()
+fruits = {"apple", "banana", "cherry"}
+fruits.pop() 
+print(fruits)
+
+fruits = {"apple", "banana", "cherry"}
+x = fruits.pop()
+print(x)
+
+# remove()
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+print(fruits)
+
+# symmetric_difference()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.symmetric_difference(y)
+print(z)
+
+# symmetric_difference_update()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.symmetric_difference_update(y)
+print(x)
+
+# union()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.union(y)
+print(z)
+
+x = {"a", "b", "c"}
+y = {"f", "d", "a"}
+z = {"c", "d", "e"}
+result = x.union(y, z)
+print(result)
+
+#  update()
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.update(y)
+print(x)
