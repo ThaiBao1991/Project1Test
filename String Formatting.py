68452a1ccb0682d1ddd915d4b5083973831ac378
12953 Bao <12953 Bao@vdm.mabuchi-motor.co.jp>
2023-11-23 14:03:38 +0700

    a

---
+++ String Formatting.py
@@ -0,0 +1,9 @@
+# -****************************** String Formatting
+print("-********************** String Formatting")
+# - User Input
+username = input("Enter username:")
+print("Username is: " + username)
+
+# Python <=2.7
+username = raw_input("Enter username:")
+print("Username is: " + username)
