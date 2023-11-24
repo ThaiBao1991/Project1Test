fa26e2e0f207a14874cdb1526c06cf089c1f6bba
12953 Bao <12953 Bao@vdm.mabuchi-motor.co.jp>
2023-11-24 10:49:00 +0700

    sa

---
+++ File Handling/FileHanding.py
@@ -0,0 +1,12 @@
+# -****************************** File Handing
+print("-********************** File Handing")
+f = open("demofile.txt", "r")
+print(f.read())
+
+# -Open a File on the Server
+print("-Open a File on the Server")
+f = open("demofile.txt", "r")
+print(f.read())
+
+f = open("D:\\myfiles\welcome.txt", "r")
+print(f.read())
--- String Formatting.py
+++
@@ -1,9 +0,0 @@
-# -****************************** String Formatting
-print("-********************** String Formatting")
-# - User Input
-username = input("Enter username:")
-print("Username is: " + username)
-
-# Python <=2.7
-username = raw_input("Enter username:")
-print("Username is: " + username)
