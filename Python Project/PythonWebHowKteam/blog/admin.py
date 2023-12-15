52c7133b902d40f9790a8e46224e7caea0143288
12953 Bao <12953 Bao@vdm.mabuchi-motor.co.jp>
2023-12-15 20:20:44 +0700

    a

--- Python Project/PythonWebHowKteam/blog/admin.py
+++ Python Project/PythonWebHowKteam/blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date']
+    list_filter = ['date']
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
