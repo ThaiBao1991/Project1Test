ae63a2fdbd4bbab8162f12b4b233463f4d7aaea6
Thai Bao <thaibao1991dn@gmail.com>
2023-12-26 00:04:15 +0700

    a

--- Python Project/PDF encrypt/Version2.py
+++ Python Project/PDF encrypt/Version2.py
@@ -3,8 +3,9 @@ import PyPDF2
 import tkinter as tk
 from tkinter import filedialog
 import os
+import glob
 
-def encrypt_pdf(input_file, output_file):
+def encrypt_pdf(input_file, output_file, password):
     # Đọc file PDF nguồn
     with open(input_file, 'rb') as file:
         pdf_reader = PyPDF2.PdfFileReader(file)
@@ -16,20 +17,20 @@ def encrypt_pdf(input_file, output_file):
             pdf_writer.addPage(page)
 
         # Mã hóa file PDF đã đọc
-        encrypted_pdf = pdf_writer.encrypt('password')
+        encrypted_pdf = pdf_writer.encrypt(password)
 
         # Tạo file mới và ghi dữ liệu mã hóa vào file
         with open(output_file, 'wb') as encrypted_file:
             pdf_writer.write(encrypted_file)
 
-def decrypt_pdf(input_file, output_file):
+def decrypt_pdf(input_file, output_file, password):
     # Đọc file mã hóa
     with open(input_file, 'rb') as encrypted_file:
         pdf_reader = PyPDF2.PdfFileReader(encrypted_file)
 
         # Giải mã file PDF
         if pdf_reader.isEncrypted:
-            pdf_reader.decrypt('password')
+            pdf_reader.decrypt(password)
 
         # Tạo file mới và ghi dữ liệu giải mã vào file
         with open(output_file, 'wb') as decrypted_file:
@@ -41,6 +42,38 @@ def decrypt_pdf(input_file, output_file):
 
             pdf_writer.write(decrypted_file)
 
+def encrypt_folder():
+    folder_path = filedialog.askdirectory()
+    password = 'password'  # Mật khẩu để mã hóa
+
+    # Lấy danh sách tất cả các file PDF trong thư mục
+    pdf_files = glob.glob(os.path.join(folder_path, '*.pdf'))
+
+    for pdf_file in pdf_files:
+        # Tạo đường dẫn đầu ra cho file mã hóa (.ktb)
+        output_file = os.path.splitext(pdf_file)[0] + '.ktb'
+
+        # Mã hóa file PDF
+        encrypt_pdf(pdf_file, output_file, password)
+
+    result_label['text'] = 'Folder encrypted successfully: ' + folder_path
+
+def decrypt_folder():
+    folder_path = filedialog.askdirectory()
+    password = 'password'  # Mật khẩu để giải mã
+
+    # Lấy danh sách tất cả các file mã hóa (.ktb) trong thư mục
+    encrypted_files = glob.glob(os.path.join(folder_path, '*.ktb'))
+
+    for encrypted_file in encrypted_files:
+        # Tạo đường dẫn đầu ra cho file giải mã (PDF)
+        output_file = os.path.splitext(encrypted_file)[0] + '.pdf'
+
+        # Giải mã file PDF
+        decrypt_pdf(encrypted_file, output_file, password)
+
+    result_label['text'] = 'Folder decrypted successfully: ' + folder_path
+
 def open_file():
     file_path = filedialog.askopenfilename(filetypes=[('PDF Files', '*.pdf')])
     input_entry.delete(0, tk.END)
@@ -49,13 +82,15 @@ def open_file():
 def encrypt_file():
     input_file = input_entry.get()
     output_file = os.path.splitext(input_file)[0] + '.bkt'
-    encrypt_pdf(input_file, output_file)
+    password = 'password'  # Mật khẩu để mã hóa
+    encrypt_pdf(input_file, output_file, password)
     result_label['text'] = 'File encrypted successfully: ' + output_file
 
 def decrypt_file():
     input_file = input_entry.get()
     output_file = os.path.splitext(input_file)[0] + '.pdf'
-    decrypt_pdf(input_file, output_file)
+    password = 'password'  # Mật khẩu để giải mã
+    decrypt_pdf(input_file, output_file, password)
     result_label['text'] = 'File decrypted successfully: ' + output_file
 
 # Tạo giao diện ứng dụng
@@ -77,6 +112,12 @@ encrypt_button.pack()
 
 decrypt_button = tk.Button(root, text='Decrypt File', command=decrypt_file)
 decrypt_button.pack()
+
+encrypt_folder_button = tk.Button(root, text='Encrypt Folder', command=encrypt_folder)
+encrypt_folder_button.pack()
+
+decrypt_folder_button = tk.Button(root, text='Decrypt Folder', command=decrypt_folder)
+decrypt_folder_button.pack()
 
 result_label = tk.Label(root, text='')
 result_label.pack()
