0962f71f5df3a4705f92c91132c72e34b9f00151
12953 Bao <12953 Bao@vdm.mabuchi-motor.co.jp>
2023-12-27 18:32:09 +0700

    a

--- Python Project/OnlineSchoolMgmt/app/admin.py
+++ Python Project/OnlineSchoolMgmt/app/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
  
 # Register your models here.
-admin.site.register(Atttendance)
+admin.site.register(Attendance)
 admin.site.register(Notice)
 admin.site.register(Marks)
